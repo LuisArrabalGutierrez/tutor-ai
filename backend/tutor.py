90adb50aab47a3ed4f25a733455f2197821b0c3e
LuisArrabalGutierrez <luisarrabalgutierrez@gmail.com>
2026-04-30 18:01:21 +0200

    modelo llm cambiado 2

--- backend/tutor.py
+++ backend/tutor.py
@@ -15,8 +15,8 @@ load_dotenv()
 # Inicia LLM
 llm = ChatGroq(
     api_key=os.getenv("GROQ_API_KEY"),
-    model_name="llama-3.3-70b-versatile" 
-    #model_name="llama-3.1-8b-instant"
+    #model_name="llama-3.3-70b-versatile" 
+    model_name="llama-3.1-8b-instant"
     #model_name="openai/gpt-oss-120b"
 
 )
